e6a7dd4dcd707c7426b8347b57ea5df256e1c569
Alexey Gladkov <legion@altlinux.org>
2018-09-10 21:19:13 +0200

    Replace "~" by home directory
    Signed-off-by: Alexey Gladkov <legion@altlinux.org>

--- internal/config/file.go
+++ internal/config/file.go
@@ -2,10 +2,21 @@ package config
 
 import (
 	"os"
+	"path/filepath"
+	"strings"
 	"time"
 
 	"gopkg.in/yaml.v2"
 )
+
+func substHome(s string) string {
+	arr := strings.Split(s, string(os.PathSeparator))
+	if arr[0] == "~" {
+		arr[0] = os.Getenv("HOME")
+		return filepath.Join(arr...)
+	}
+	return s
+}
 
 func Read(filename string) (*Configuration, error) {
 	cfg := &Configuration{}
@@ -21,6 +32,12 @@ func Read(filename string) (*Configuration, error) {
 	err = yaml.NewDecoder(f).Decode(cfg)
 	if err != nil {
 		return nil, err
+	}
+
+	cfg.Core.LogFile = substHome(cfg.Core.LogFile)
+	cfg.Core.LockDir = substHome(cfg.Core.LockDir)
+	for name := range cfg.Remote {
+		cfg.Remote[name].DestDir = substHome(cfg.Remote[name].DestDir)
 	}
 
 	return cfg, nil
